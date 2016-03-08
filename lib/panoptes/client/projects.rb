355188efcc05d720130fbf39bdd536d7c0211d28
Marten Veldthuis <marten@veldthuis.com>
2016-03-08 14:57:36 +0000

    Add other export types

--- lib/panoptes/client/projects.rb
+++ lib/panoptes/client/projects.rb
@@ -13,20 +13,49 @@ module Panoptes
         paginate("/projects", params)["projects"]
       end
 
-      # Starts a background process to generate a new CSV export of all the classifications
-      # in the project.
+      # Starts a background process to generate a new CSV export of all the classifications in the project.
       #
       # @param project_id [Integer] the id of the project to export
-      # @return [Hash] the medium information where the project will be stored when it's generated
+      # @return [Hash] the medium information where the export will be stored when it's generated
       def create_classifications_export(project_id)
-        params = {
-          media: {
-            content_type: "text/csv",
-            metadata: { recipients: [] }
-          }
-        }
-
+        params = {media: {content_type: "text/csv", metadata: { recipients: []}}}
         post("/projects/#{project_id}/classifications_export", params)["media"].first
+      end
+
+      # Starts a background process to generate a new CSV export of all the subjects in the project.
+      #
+      # @param project_id [Integer] the id of the project to export
+      # @return [Hash] the medium information where the export will be stored when it's generated
+      def create_subjects_export(project_id)
+        params = {media: {content_type: "text/csv", metadata: { recipients: []}}}
+        post("/projects/#{project_id}/subjects_export", params)["media"].first
+      end
+
+      # Starts a background process to generate a new CSV export of all the workflows in the project.
+      #
+      # @param project_id [Integer] the id of the project to export
+      # @return [Hash] the medium information where the export will be stored when it's generated
+      def create_workflows_export(project_id)
+        params = {media: {content_type: "text/csv", metadata: { recipients: []}}}
+        post("/projects/#{project_id}/workflows_export", params)["media"].first
+      end
+
+      # Starts a background process to generate a new CSV export of all the workflow_contents in the project.
+      #
+      # @param project_id [Integer] the id of the project to export
+      # @return [Hash] the medium information where the export will be stored when it's generated
+      def create_workflow_contents_export(project_id)
+        params = {media: {content_type: "text/csv", metadata: { recipients: []}}}
+        post("/projects/#{project_id}/workflow_contents_export", params)["media"].first
+      end
+
+      # Starts a background process to generate a new CSV export of the aggretation results of the project.
+      #
+      # @param project_id [Integer] the id of the project to export
+      # @return [Hash] the medium information where the export will be stored when it's generated
+      def create_aggregations_export(project_id)
+        params = {media: {content_type: "application/x-gzip", metadata: { recipients: []}}}
+        post("/projects/#{project_id}/aggregations_export", params)["media"].first
       end
     end
   end
--- spec/panoptes/client/projects_spec.rb
+++ spec/panoptes/client/projects_spec.rb
@@ -24,15 +24,81 @@ describe Panoptes::Client::Projects, :vcr do
       export = client.create_classifications_export(813)
 
       expect(export).to include({
-        "id"=>"38663", 
         "href"=>"/projects/813/classifications_export", 
         "content_type"=>"text/csv", 
         "media_type"=>"project_classifications_export", 
         "external_link"=>false, 
-        "metadata"=>{"recipients"=>[]}, 
         "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
       })
-      expect(export["src"]).to include("https://zooniverse-static.s3.amazonaws.com/panoptes-uploads.zooniverse.org/staging/project_classifications_export/")
+      expect(export["src"]).to include("https://panoptes-uploads.zooniverse.org/staging/project_classifications_export/")
+    end
+  end
+
+  describe '#create_subjects_export' do
+    let(:client) { user_client }
+
+    it 'returns the export medium hash' do
+      export = client.create_subjects_export(813)
+
+      expect(export).to include({
+        "href"=>"/projects/813/subjects_export", 
+        "content_type"=>"text/csv", 
+        "media_type"=>"project_subjects_export", 
+        "external_link"=>false, 
+        "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
+      })
+      expect(export["src"]).to include("https://panoptes-uploads.zooniverse.org/staging/project_subjects_export/")
+    end
+  end
+
+  describe '#create_workflows_export' do
+    let(:client) { user_client }
+
+    it 'returns the export medium hash' do
+      export = client.create_workflows_export(813)
+
+      expect(export).to include({
+        "href"=>"/projects/813/workflows_export", 
+        "content_type"=>"text/csv", 
+        "media_type"=>"project_workflows_export", 
+        "external_link"=>false, 
+        "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
+      })
+      expect(export["src"]).to include("https://panoptes-uploads.zooniverse.org/staging/project_workflows_export/")
+    end
+  end
+
+  describe '#create_workflow_contents_export' do
+    let(:client) { user_client }
+
+    it 'returns the export medium hash' do
+      export = client.create_workflow_contents_export(813)
+
+      expect(export).to include({
+        "href"=>"/projects/813/workflow_contents_export", 
+        "content_type"=>"text/csv", 
+        "media_type"=>"project_workflow_contents_export", 
+        "external_link"=>false, 
+        "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
+      })
+      expect(export["src"]).to include("https://panoptes-uploads.zooniverse.org/staging/project_workflow_contents_export/")
+    end
+  end
+
+  describe '#create_aggregations_export' do
+    let(:client) { user_client }
+
+    it 'returns the export medium hash' do
+      export = client.create_aggregations_export(813)
+
+      expect(export).to include({
+        "href"=>"/projects/813/aggregations_export", 
+        "content_type"=>"application/x-gzip", 
+        "media_type"=>"project_aggregations_export", 
+        "external_link"=>false, 
+        "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
+      })
+      expect(export["src"]).to include("https://panoptes-uploads.zooniverse.org/staging/project_aggregations_export/")
     end
   end
 end
