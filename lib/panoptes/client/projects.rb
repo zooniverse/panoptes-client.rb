c82d291fa28ede314fbf17ca8ad3a858f66f3dec
Marten Veldthuis <marten@veldthuis.com>
2016-03-08 14:37:00 +0000

    Add classifications export API call

--- lib/panoptes/client/projects.rb
+++ lib/panoptes/client/projects.rb
@@ -12,6 +12,22 @@ module Panoptes
 
         paginate("/projects", params)["projects"]
       end
+
+      # Starts a background process to generate a new CSV export of all the classifications
+      # in the project.
+      #
+      # @param project_id [Integer] the id of the project to export
+      # @return [Hash] the medium information where the project will be stored when it's generated
+      def create_classifications_export(project_id)
+        params = {
+          media: {
+            content_type: "text/csv",
+            metadata: { recipients: [] }
+          }
+        }
+
+        post("/projects/#{project_id}/classifications_export", params)["media"].first
+      end
     end
   end
 end
--- spec/panoptes/client/projects_spec.rb
+++ spec/panoptes/client/projects_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe Panoptes::Client::Projects do
-  describe 'unauthenticated', :vcr do
+describe Panoptes::Client::Projects, :vcr do
+  describe 'unauthenticated' do
     let(:client) { unauthenticated_client }
 
     it 'returns a list of projects' do
@@ -14,6 +14,25 @@ describe Panoptes::Client::Projects do
       projects = client.projects(search: "galaxy")
       names = projects.map { |project| project["display_name"] }
       expect(names.all? { |name| name.downcase.include?("galaxy") }).to be_truthy
+    end
+  end
+
+  describe '#create_classifications_export' do
+    let(:client) { user_client }
+
+    it 'returns the export medium hash' do
+      export = client.create_classifications_export(813)
+
+      expect(export).to include({
+        "id"=>"38663", 
+        "href"=>"/projects/813/classifications_export", 
+        "content_type"=>"text/csv", 
+        "media_type"=>"project_classifications_export", 
+        "external_link"=>false, 
+        "metadata"=>{"recipients"=>[]}, 
+        "links"=>{"linked"=>{"href"=>"/projects/813", "id"=>"813", "type"=>"projects"}}
+      })
+      expect(export["src"]).to include("https://zooniverse-static.s3.amazonaws.com/panoptes-uploads.zooniverse.org/staging/project_classifications_export/")
     end
   end
 end
