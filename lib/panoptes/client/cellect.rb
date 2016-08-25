535d95d296de5a2480bca38a8e2e83ed380796ca
Michael Parrish <mtparrish@gmail.com>
2016-08-25 05:49:24 -0500

    Add Cellect endpoints (#7)
    * Refactor and add cellect endpoints
    
    * Update documentation

---
+++ lib/panoptes/client/cellect.rb
@@ -0,0 +1,22 @@
+require 'faraday'
+
+module Panoptes
+  class Client
+    module Cellect
+      # Fetches all cellect-enabled, launched workflows.
+      #
+      # @return [Hash] the list of workflows
+      def cellect_workflows
+        cellect.get '/workflows'
+      end
+
+      # Fetches all active subjects for a cellect-enabled workflow.
+      #
+      # @param workflow_id [Integer] the id of the workflow
+      # @return [Hash] the list of subjects
+      def cellect_subjects(workflow_id)
+        cellect.get '/subjects', workflow_id: workflow_id
+      end
+    end
+  end
+end
---
+++ lib/panoptes/endpoints/json_endpoint.rb
@@ -0,0 +1,18 @@
+require_relative 'base_endpoint'
+
+module Panoptes
+  module Endpoints
+    class JsonEndpoint < BaseEndpoint
+      # Automatically configured connection to use JSON requests/responses
+      # @see Panoptes::Endpoints::BaseEndpoint#initialize
+      def initialize(auth: {}, url: nil, prefix: nil, &config)
+        super auth: auth, url: url, prefix: prefix do |faraday|
+          config&.call faraday
+          faraday.request :json
+          faraday.response :json
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+    end
+  end
+end
