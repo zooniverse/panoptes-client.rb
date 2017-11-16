c65bc3b56666b7ae11838ab36c9886a96bb8343f
Amy Boyer <amy@zooniverse.org>
2017-11-16 14:15:05 -0600

    add a spec

--- spec/panoptes/client/classifications_spec.rb
+++ spec/panoptes/client/classifications_spec.rb
@@ -9,4 +9,15 @@ describe Panoptes::Client::Classifications, :vcr do
       assert_requested :get, api_url("/classifications/project?admin=true&workflow_id=5678&subject_id=1234")
     end
   end
+
+  describe '#get_user_classifications' do
+    let(:client){ user_client }
+
+    it 'gets some classifications' do
+      result = client.get_user_classifications(1325464, 2218)
+      assert_requested :get, api_url("/classifications/project?admin=true&workflow_id=2218&user_id=1325464")
+      expect(result["classifications"].count).to eq(2)
+      expect(result["classifications"][0]["id"]).to eq("88262")
+    end
+  end
 end
