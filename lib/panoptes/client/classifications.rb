0cb62126bd44107f3640ca2237d87ad117c7c606
Marten Veldthuis <marten@veldthuis.com>
2017-11-16 16:08:48 +0000

    Paginate fetched classifications

--- lib/panoptes/client/classifications.rb
+++ lib/panoptes/client/classifications.rb
@@ -2,11 +2,11 @@ module Panoptes
   class Client
     module Classifications
       def get_subject_classifications(subject_id, workflow_id)
-        panoptes.get("/classifications/project", {
+        panoptes.paginate("/classifications/project", {
           admin: true,
           workflow_id: workflow_id,
           subject_id: subject_id
-        })
+        }, resource: "classifications")
       end
     end
   end
