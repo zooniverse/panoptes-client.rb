604f0a5938d0d2907d533371d06cb7649fb5c0a8
Amy Boyer <amy@zooniverse.org>
2017-11-16 12:33:24 -0600

    add method to fetch classifications for a given user in a given workflow

--- lib/panoptes/client/classifications.rb
+++ lib/panoptes/client/classifications.rb
@@ -8,6 +8,14 @@ module Panoptes
           subject_id: subject_id
         }, resource: "classifications")
       end
+
+      def get_user_classifications(user_id, workflow_id)
+        panoptes.paginate("/classifications/project", {
+          admin: true,
+          workflow_id: workflow_id,
+          user_id: user_id
+        }, resource: "classifications")
+      end
     end
   end
 end
