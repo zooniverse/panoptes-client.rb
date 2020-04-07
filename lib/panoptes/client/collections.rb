b4b3cccb699cf428f59dc154897ef4215d44f031
Nicole Ciemniak <nciemniak@gmail.com>
2020-04-07 13:59:13 -0500

    add get collection method

--- lib/panoptes/client/collections.rb
+++ lib/panoptes/client/collections.rb
@@ -3,6 +3,11 @@
 module Panoptes
   class Client
     module Collections
+      def collection(collection_id)
+        response = panoptes.get("/collections/#{collection_id}")
+        response.fetch('collections').find { |i| i.fetch('id').to_s == collection_id.to_s }
+      end
+
       def add_subjects_to_collection(collection_id, subject_ids)
         panoptes.post("/collections/#{collection_id}/links/subjects", subjects: subject_ids)
       end
--- spec/panoptes/client/collections_spec.rb
+++ spec/panoptes/client/collections_spec.rb
@@ -3,9 +3,17 @@
 require 'spec_helper'
 
 describe Panoptes::Client::Collections, :vcr do
-  describe '#add_subjects_to_collection' do
-    let(:client) { user_client }
+  let(:client) { user_client }
 
+  describe '#collection' do
+    it 'returns a collection with the given id' do
+      collection = client.collection(1)
+      expect(collection['id']).to eq('1')
+      assert_requested :get, api_url('/collections/1')
+    end
+  end
+
+  describe '#add_subjects_to_collection' do
     it 'adds subjects' do
       client.add_subjects_to_collection(1234, [4567])
       assert_requested :post, api_url('/collections/1234/links/subjects')
