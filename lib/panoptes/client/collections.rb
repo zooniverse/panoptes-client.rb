100de31d0dbfc52c7cc7a51b40c2f446fbf4715d
nciemniak <nciemniak@gmail.com>
2020-04-08 11:04:20 -0500

    add get single collection method (#34)
    * add get collection method

--- lib/panoptes/client/collections.rb
+++ lib/panoptes/client/collections.rb
@@ -3,6 +3,11 @@
 module Panoptes
   class Client
     module Collections
+      def collection(collection_id)
+        response = panoptes.get("/collections/#{collection_id}")
+        response['collections'].first
+      end
+
       def add_subjects_to_collection(collection_id, subject_ids)
         panoptes.post("/collections/#{collection_id}/links/subjects", subjects: subject_ids)
       end
