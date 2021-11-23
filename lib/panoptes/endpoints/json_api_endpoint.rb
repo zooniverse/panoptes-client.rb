03d3c80e97d881c2f5fdba08057a774d29fab7a0
Campbell Allen <campbell.allen@gmail.com>
2021-11-23 15:56:33 +0000

    fix pagination behaviour
    ensure we yield the first page and then subsequent pages

--- lib/panoptes/endpoints/json_api_endpoint.rb
+++ lib/panoptes/endpoints/json_api_endpoint.rb
@@ -10,17 +10,25 @@ module Panoptes
         resource = path.split('/').last if resource.nil?
         data = last_response = get(path, query)
 
-        while next_path = last_response['meta'][resource]['next_href']
+        # ensure we yield the first page of data
+        yield data, last_response if block_given?
+
+        # while we have more result set pages
+        while (next_path = last_response['meta'][resource]['next_href'])
+          # fetch subsequent pages of data
           last_response = get(next_path, query)
           if block_given?
+            # if we pass a block then yield the first page and current page responses
             yield data, last_response
           else
+            # add to the data representation of all result set pages
             data[resource].concat(last_response[resource]) if data[resource].is_a?(Array)
             data['meta'][resource].merge!(last_response['meta'][resource])
             data['links'].merge!(last_response['links'])
           end
         end
 
+        # return the data results for when we don't use a block to process response pages
         data
       end
     end
